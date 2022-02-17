6dbf980364f4f0355b5a5cc99236161a15102e72
Pavel Ryapolov <paulustg@gmail.com>
2022-02-17 20:50:58 +0400

    Описаны маршруты для приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,49 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainPage from '../main-page/main-page';
+import LoginPage from '../login-page/login-page';
+import FavoritesPage from '../favorites-page/favorites-page';
+import OfferPage from '../offer-page/offer-page';
+import NotFoundPage from '../not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placesCount: number;
 }
 
 function App({ placesCount }: AppProps): JSX.Element {
-  return <MainPage placesCount={placesCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<OfferPage />}
+        />
+        <Route
+          path='*'
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
